1f8af2f9c10c4e52047c496d7c82649e2a75e58f
Vinicius <viniciusribeiro.dev@hotmail.com>
2023-03-09 11:38:37 -0300

    fix: index.html and routes

---
+++ projeto-frontEnd-TeamSix/src/API/index.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: 'https://teamsixfilms.onrender.com',
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'appication/json'
+  }
+}) 
